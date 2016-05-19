863d3637c218d1416bd67e518677ce6660f2a07a
Danilo Vitoriano <vitoriano08@gmail.com>
2016-05-18 23:53:23 -0300

    package record

--- src/HUAM/FormMain.java
+++ src/HUAM/FormMain.java
@@ -7,6 +7,7 @@ package HUAM;
 
 import user.UserListForm;
 import patient.PatientListForm;
+import record.RecordListForm;
 
 /**
  *
@@ -32,6 +33,7 @@ public class FormMain extends javax.swing.JFrame {
 
         jButtonUsuario = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,6 +51,13 @@ public class FormMain extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Anamnese");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -56,9 +65,10 @@ public class FormMain extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(159, 159, 159)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2)
                     .addComponent(jButton1)
                     .addComponent(jButtonUsuario))
-                .addContainerGap(179, Short.MAX_VALUE))
+                .addContainerGap(156, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -67,7 +77,9 @@ public class FormMain extends javax.swing.JFrame {
                 .addComponent(jButtonUsuario)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addContainerGap(165, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(118, Short.MAX_VALUE))
         );
 
         pack();
@@ -84,6 +96,12 @@ public class FormMain extends javax.swing.JFrame {
         PatientListForm form = new PatientListForm();
         form.show();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        RecordListForm form = new RecordListForm();
+        form.show();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,6 +140,7 @@ public class FormMain extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButtonUsuario;
     // End of variables declaration//GEN-END:variables
 }
